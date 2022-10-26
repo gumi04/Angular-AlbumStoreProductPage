dc65af6c6c76973725a836b8a7b2df5e6d4f98af
Gumaro Contreras Casillas <gumaro.contreras@proveedoresgnp.mx>
2022-10-26 16:25:30 -0500

    task 7  v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -10,6 +11,12 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'}
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +28,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
